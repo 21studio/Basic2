7eb4a0b52474bfc6a694102b477151ae87f1786b
21studio <dplayer155@gmail.com>
2018-07-09 17:21:16 +0900

    Initial commit

---
+++ Assets/Script/CameraControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public float sensitivity = 700.0f;
+	float rotationX;
+	float rotationY;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float mouseMoveValueX = Input.GetAxis("Mouse X");
+		float mouseMoveValueY = Input.GetAxis("Mouse Y");
+
+		rotationY += mouseMoveValueX * sensitivity * Time.deltaTime;
+		rotationX += mouseMoveValueY * sensitivity * Time.deltaTime;
+
+		rotationX %= 360;
+		rotationY %= 360;
+		
+		//Debug.Log("rotation X : " + rotationX + " / " + "rotation Y : " + rotationY, gameObject);
+		
+		rotationX = Mathf.Clamp(rotationX, -27f, 27f);
+		
+		/* 
+		if (rotationX > 27) {
+			rotationX = 27;
+		}
+		else if (rotationX < -27) {
+			rotationX = -27;
+		} */
+		
+		transform.eulerAngles = new Vector3(-rotationX, rotationY, 0.0f);
+
+		if (Input.GetKeyDown(KeyCode.A)) {
+			Debug.LogWarning("Warning");
+		}
+		if (Input.GetKeyDown(KeyCode.B)) {
+			Debug.LogError("Error");
+		}
+
+	}
+}
---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	CharacterController characterController = null;
+
+	public Transform cameraTransform;
+	public float moveSpeed = 10.0f;
+
+	public float jumpSpeed = 10.0f;
+	public float gravity = -20.0f;
+
+	float yVelocity = 0.0f;
+	int jumpCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		Vector3 moveDirection = new Vector3(x, 0, z);
+		moveDirection = cameraTransform.TransformDirection(moveDirection);
+		moveDirection *= moveSpeed;
+
+		if (characterController.isGrounded == true) {
+			yVelocity = 0.0f;
+			jumpCount = 0;
+		}
+		//Debug.Log(characterController.isGrounded);
+		
+		if (Input.GetButtonDown("Jump") && jumpCount < 2) {
+			yVelocity = jumpSpeed;
+			
+			jumpCount ++;			
+		}
+
+		yVelocity += (gravity * Time.deltaTime);
+		moveDirection.y = yVelocity;
+
+		Debug.Log(yVelocity);
+
+		characterController.Move(moveDirection * Time.deltaTime);
+		
+	}
+}
