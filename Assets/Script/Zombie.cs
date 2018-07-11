8144fc700eaa7a30ba3be1946233d1e472c9e5fa
21studio <dplayer155@gmail.com>
2018-07-11 12:58:39 +0900

    180711 @mac launch

--- Assets/Script/Zombie.cs
+++ Assets/Script/Zombie.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour {
 
+    float stateTime = 0.0f;
+    public float idleStateMaxTime = 2.0f;
+    public Animation anim;
+
+    Transform target = null;
+    CharacterController characterController = null;
+
+    public float moveSpeed = 5.0f;
+    public float rotationSpeed = 10.0f;
+    public float attackRange = 2.5f;
+
 	enum ENEMYSTATE {
 		IDLE = 0,
 		MOVE,
@@ -14,17 +25,54 @@ public class Zombie : MonoBehaviour {
 
 	ENEMYSTATE enemyState = ENEMYSTATE.IDLE;
 
-	void Update () {
+	void Awake () {
+        InitZombie();
+    }
+
+    void Start () {
+        target = GameObject.FindWithTag("Player").transform;
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void InitZombie() {
+        enemyState = ENEMYSTATE.IDLE;
+        PlayIdleAnim();
+    }
+
+    void PlayIdleAnim() {
+        anim["Idle"].speed = 3.0f;
+        anim.Play("Idle");
+    }
+    
+    void Update () {
         switch (enemyState)
         {
             case ENEMYSTATE.IDLE:
                 {
-                    // idle
+                    stateTime += Time.deltaTime;
+                    if (stateTime > idleStateMaxTime) {
+                        stateTime = 0.0f;
+                        enemyState = ENEMYSTATE.MOVE;
+                    }
                 }
                 break;
             case ENEMYSTATE.MOVE:
                 {
+                    anim["Move"].speed = 2.0f;
+                    anim.CrossFade("Move");
 
+                    float distance = (target.position - transform.position).magnitude;
+                    if (distance < attackRange) {
+                        enemyState = ENEMYSTATE.ATTACK;
+                    }
+                    else {
+                        Vector3 dir = target.position - transform.position;
+                        dir.y = 0.0f;
+                        dir.Normalize();
+                        characterController.SimpleMove(dir * moveSpeed);
+
+                        transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation(dir), rotationSpeed * Time.deltaTime);
+                    }
                 }
                 break;
             case ENEMYSTATE.ATTACK:
